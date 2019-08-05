5a56b87bfe83c4cb37116106400813e425d58c1e
Adhair Alejandro Tapia Arias <gilloadaflow@gmail.com>
2019-08-05 20:47:36 +0000

    Automatically backed up by Learn

--- current_age_for_birth_year.rb
+++ current_age_for_birth_year.rb
@@ -1,7 +1,4 @@
-def current_age_for_birth_year(birth_year)
-2013 - birth_year
+def current_age_for_birth_year (birth_year)
+2003 - birth_year
 
 end
-
-
-age_of_person = current_age_for_birth_year(1984)
